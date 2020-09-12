9c5e5a1a0565e3394f143893a0f38bbec4cb6123
Pooja Rathore <poojarathore.nks@gmail.com>
2020-09-13 01:38:21 +0530

    YounGold Initial BUild

---
+++ client/build/precache-manifest.ebab55ec056e6a51fd99afd52a66d445.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79c6c02c1f07c8ef34fe46870348572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d29a1f3bef04c458072c",
+    "url": "/static/css/main.61ca23db.chunk.css"
+  },
+  {
+    "revision": "078329dfc19443c0a541",
+    "url": "/static/js/2.90d11d6b.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.90d11d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29a1f3bef04c458072c",
+    "url": "/static/js/main.f63ef37d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "37f223fc792281310c8760fc7342a749",
+    "url": "/static/media/YG.37f223fc.PNG"
+  },
+  {
+    "revision": "1fa6ecb807e693badb0175d844312533",
+    "url": "/static/media/new.1fa6ecb8.jpeg"
+  },
+  {
+    "revision": "0b4e28dae1aa3b0826bc290aad39b797",
+    "url": "/static/media/welcome.0b4e28da.PNG"
+  }
+]);
---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
+import { getProfiles } from '../../actions/profile';
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, [getProfiles]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className='large text-primary'>Community</h1>
+          <a className='recommend'  href='https://youngold.herokuapp.com/'><button className='btn btn-primary'>Recommend Profiles</button></a>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop' /> Browse and Make Connection to seek help 
+          </p>
+          <div className='profiles'>
+            {profiles.length > 0 ? (
+              profiles.map(profile => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
